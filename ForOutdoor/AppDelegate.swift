31cbc4254e9afe1522bfad390c4df88d829c3aab
Tosha <tosha9@gmail.com>
2015-04-11 16:20:39 -0500

    first commit

--- ForOutdoor/AppDelegate.swift
+++ ForOutdoor/AppDelegate.swift
@@ -1,46 +1,155 @@
-//
-//  AppDelegate.swift
-//  ForOutdoor
-//
-//  Created by Tosha on 4/10/15.
-//  Copyright (c) 2015 NICUSA. All rights reserved.
-//
-
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+    
     var window: UIWindow?
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    var locationManager: CLLocationManager?
+    var lastProximity: CLProximity?
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool
+    {
+        
+            
+            return true
     }
-
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
+        
+        let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+        let beaconIdentifier = "estimote"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)!
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID,
+            identifier: beaconIdentifier)
+        
+        locationManager = CLLocationManager()
+        
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+        
+        if(application.respondsToSelector("registerUserNotificationSettings:")) {
+            application.registerUserNotificationSettings(
+                UIUserNotificationSettings(
+                    forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound,
+                    categories: nil
+                )
+            )
+        }
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    
 }
 
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!, playSound: Bool) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        
+        if(playSound) {
+            // classic star trek communicator beep
+            //	http://www.trekcore.com/audio/
+            //
+            // note: convert mp3 and wav formats into caf using:
+            //	"afconvert -f caff -d LEI16@44100 -c 1 in.wav out.caf"
+            // http://stackoverflow.com/a/10388263
+            
+            notification.soundName = "tos_beep.caf";
+        }
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didRangeBeacons beacons: [AnyObject]!,
+        inRegion region: CLBeaconRegion!) {
+          /*  let viewController:ViewController = window!.rootViewController as! ViewController
+            viewController.beacons = beacons as [CLBeacon]?
+            viewController.tableView!.reloadData()*/
+            
+            NSLog("didRangeBeacons");
+            var message:String = ""
+            
+            var playSound = false
+            
+            if(beacons.count > 0) {
+                let nearestBeacon:CLBeacon = beacons[0] as! CLBeacon
+                
+                if(nearestBeacon.proximity == lastProximity ||
+                    nearestBeacon.proximity == CLProximity.Unknown) {
+                        return;
+                }
+                lastProximity = nearestBeacon.proximity;
+                
+                switch nearestBeacon.proximity {
+                case CLProximity.Far:
+                    message = "You are far away from the beacon"
+                    playSound = true
+                case CLProximity.Near:
+                    message = "You are near the beacon"
+                case CLProximity.Immediate:
+                    message = "You are in the immediate proximity of the beacon"
+                case CLProximity.Unknown:
+                    return
+                }
+            } else {
+                
+                if(lastProximity == CLProximity.Unknown) {
+                    return;
+                }
+                
+                message = "No beacons are nearby"
+                playSound = true
+                lastProximity = CLProximity.Unknown
+            }
+            
+            NSLog("%@", message)
+            sendLocalNotificationWithMessage(message, playSound: playSound)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didEnterRegion region: CLRegion!) {
+            manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.startUpdatingLocation()
+            
+            NSLog("You entered the region")
+            sendLocalNotificationWithMessage("You entered the region", playSound: false)
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didExitRegion region: CLRegion!) {
+            manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.stopUpdatingLocation()
+            
+            NSLog("You exited the region")
+            sendLocalNotificationWithMessage("You exited the region", playSound: true)
+    }
+}
--- ForOutdoor/ViewController.swift
+++ ForOutdoor/ViewController.swift
@@ -7,19 +7,84 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D"), identifier: "Estimotes")
+    
+    
+    @IBOutlet weak var beaconLabel: UILabel!
+    
+    
+    
+    // Note: make sure you replace the keys here with your own beacons' Minor Values
+    let colors = [
+        8677: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1), // Dark blue
+        9738: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1), // Light blue
+        59272: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)//Mint
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        if (CLLocationManager.authorizationStatus() != CLAuthorizationStatus.AuthorizedWhenInUse) {
+            locationManager.requestWhenInUseAuthorization()
+        }
+        locationManager.startRangingBeaconsInRegion(region)
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    func locationManager(manager: CLLocationManager!,
+        didEnterRegion region: CLRegion!) {
+            manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.startUpdatingLocation()
+            
+            NSLog("You entered the region")
+        //    sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!,
+        didExitRegion region: CLRegion!) {
+            manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
+            manager.stopUpdatingLocation()
+            
+            NSLog("You exited the region")
+           // sendLocalNotificationWithMessage("You exited the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        println(knownBeacons.count)
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as! CLBeacon
+            self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+            
+            switch closestBeacon.minor.integerValue
+            {
+                case 9738:
+                beaconLabel.text = "Hey the water is in proximity"
+                
+            case 8677:
+                beaconLabel.text = "Keep hiking almost there"
+                
+            case 59272:
+                beaconLabel.text = "The restroom is nearby"
+                
+            default:
+                beaconLabel.text = "Nothing to display"
+                
+            }
+            
+        
+        
+        }
+    }
 }
+
+
+
 
